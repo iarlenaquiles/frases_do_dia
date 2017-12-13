1b358368f9db5c5e5f614ff7b879123e8b93f059
Iarlen <iarlensilva775@gmail.com>
2017-12-13 09:18:20 -0300

    Criando novo estilo de texto

---
+++ index.android.js
@@ -0,0 +1,43 @@
+//Import
+import React from 'react';
+import { Text, AppRegistry, View } from 'react-native';
+
+//Formatações
+const Estilos = {
+
+  estiloTexto: {
+    fontSize: 20,
+    backgroundColor: '#08509B',
+    paddingTop: 15,
+    color: '#fff',
+    fontWeight: '900',
+    textAlign: 'center'
+  },
+  estiloTexto2: {
+    fontSize: 20,
+    backgroundColor: '#2A48FA',
+    paddingTop: 15,
+    color: '#fff',
+    fontWeight: '900',
+    textAlign: 'center'
+  },
+  estiloView:{
+    backgroundColor: 'skyblue',
+    height: 400,
+    justifyContent: 'flex-start'
+  }
+};
+//criar o componente
+const App = () => {
+
+  const { estiloTexto, estiloView, estiloTexto2 } = Estilos;
+
+  return (
+    <View style={ estiloView }>
+      <Text style={ estiloTexto } >Frases do Dia</Text>
+    </View>
+  );
+};
+//renderizar para o dispositivo
+
+AppRegistry.registerComponent('frases_do_dia', () => App);
